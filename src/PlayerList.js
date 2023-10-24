46b4755fb2cbc0411e9493b96cf828359e8539e7
fadoua <fadouadahech93@gmail.com>
2023-10-24 21:22:05 +0100

    first commit

---
+++ src/PlayerList.js
@@ -0,0 +1,16 @@
+// src/PlayerList.js
+import React from "react";
+import Player from "./Player";
+import players from "./players";
+
+const PlayerList = () => {
+  return (
+    <div >
+      {players.map((pl) => (
+        <Player {...pl} />
+      ))}
+    </div>
+  );
+};
+
+export default PlayerList;
---
+++ src/players.js
@@ -0,0 +1,36 @@
+const players =
+[
+{
+    name : "ronaldo",
+    age : 37,
+    team :"nasser",
+    nationality :"portugais",
+    jerseyNumber : 7 ,
+    image :"https://assets-fr.imgfoot.com/media/cache/1200x1200/cristiano-ronaldo-al-nassr.jpg" ,
+},
+{
+    name : "ramos",
+    age :37,
+    team :"sevilla",
+    nationality :"espaniol",
+    jerseyNumber :4 ,
+    image :"https://www.realmadrid.com/img/vertical_380px/ramos_ficha_550x650_20210630115027.jpg",
+},
+{
+    name :"benzema",
+    age : 35,
+    team :"ittihad",
+    nationality :"français" ,
+    jerseyNumber : 10 ,
+    image :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlWUCTXOz-QyfT779yWve9608KGr3g9srWgqx51QbQ9nqKazvufsM7uRfUM6zEAx3ZQro&usqp=CAU" ,
+},
+{
+    name :"vinicus jr",
+    age : 23,
+    team : "real madrid",
+    nationality :"barilian",
+    jerseyNumber : 8,
+    image :"https://www.realmadrid.com/img/vertical_380px/vinicius_380x501_20230810055554.jpg",
+},
+] ; 
+export default players ;
